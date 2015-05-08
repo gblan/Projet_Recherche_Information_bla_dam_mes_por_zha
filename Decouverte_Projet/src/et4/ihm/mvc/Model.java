a0259509033d3af8dffa9b5ecc502003499cce53
gblanc <guillaume.blanchard37@gmail.com>
2015-05-08 16:17:00 +0200

    commit local

--- Decouverte_Projet/src/et4/ihm/mvc/Model.java
+++ Decouverte_Projet/src/et4/ihm/mvc/Model.java
@@ -134,7 +134,7 @@ public class Model extends Observable {
 	 * @param desc
 	 * @return Map Sorted by value
 	 */
-	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, boolean desc) {
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, final boolean desc) {
 		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
 		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
 			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
